9b53f5f0f0d7caf507d2ad7e017c659fd2f62490
Alifiya Hussain <alifiyahussain11@gmail.com>
2019-09-17 12:22:18 +0530

    firstupload

---
+++ app/src/main/java/com/example/mynotesapp/MainActivity.java
@@ -0,0 +1,278 @@
+package com.example.mynotesapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mynotesapp.AccountActivity.LogInActivity;
+import com.example.mynotesapp.AccountActivity.SignUpActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+   /* private Button logInButton;
+    private EditText Email;
+    private EditText Password;*/
+
+    private Button btnChangePassword, btnRemoveUser,
+            changePassword, remove, signOut;
+    private TextView email;
+
+    private EditText oldEmail, password, newPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+        email = (TextView)findViewById(R.id.useremail);
+
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        setDataToView(user);
+
+        authListener = firebaseAuth -> {
+            FirebaseUser user1 = firebaseAuth.getCurrentUser();
+            if (user1 == null) {
+                // user auth state is changed - user is null
+                // launch login activity
+                startActivity(new Intent(MainActivity.this, LogInActivity.class));
+                finish();
+            }
+        };
+
+
+        btnChangePassword = (Button) findViewById(R.id.change_password_button);
+
+        btnRemoveUser = (Button) findViewById(R.id.remove_user_button);
+
+        changePassword = (Button) findViewById(R.id.changePass);
+
+        remove = (Button) findViewById(R.id.remove);
+        signOut = (Button) findViewById(R.id.sign_out);
+
+        oldEmail = (EditText) findViewById(R.id.old_email);
+
+        password = (EditText) findViewById(R.id.password);
+        newPassword = (EditText) findViewById(R.id.newPassword);
+
+        oldEmail.setVisibility(View.GONE);
+
+        password.setVisibility(View.GONE);
+        newPassword.setVisibility(View.GONE);
+
+        changePassword.setVisibility(View.GONE);
+
+        remove.setVisibility(View.GONE);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        if (progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+        }
+
+
+        btnChangePassword.setOnClickListener(v -> {
+            oldEmail.setVisibility(View.GONE);
+
+            password.setVisibility(View.GONE);
+            newPassword.setVisibility(View.VISIBLE);
+
+            changePassword.setVisibility(View.VISIBLE);
+
+            remove.setVisibility(View.GONE);
+        });
+
+        changePassword.setOnClickListener(v -> {
+            progressBar.setVisibility(View.VISIBLE);
+            if (user != null && !newPassword.getText().toString().trim().equals("")) {
+                if (newPassword.getText().toString().trim().length() < 6) {
+                    newPassword.setError("Password too short, enter minimum 6 characters");
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    user.updatePassword(newPassword.getText().toString().trim())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "Password is updated, sign in with new password!", Toast.LENGTH_SHORT).show();
+                                        signOut();
+                                        progressBar.setVisibility(View.GONE);
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Failed to update password!", Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                }
+            } else if (newPassword.getText().toString().trim().equals("")) {
+                newPassword.setError("Enter password");
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+
+        btnRemoveUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                if (user != null) {
+                    user.delete()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "Your profile is deleted:( Create a account now!", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                                        finish();
+                                        progressBar.setVisibility(View.GONE);
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Failed to delete your account!", Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setDataToView(FirebaseUser user) {
+
+        email.setText("User Email: " + user.getEmail());
+
+
+    }
+
+    // this listener will be called when there is change in firebase user session
+    FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user == null) {
+                // user auth state is changed - user is null
+                // launch login activity
+                startActivity(new Intent(MainActivity.this, LogInActivity.class));
+                finish();
+            } else {
+                setDataToView(user);
+
+            }
+        }
+
+
+    };
+
+    //sign out method
+    public void signOut() {
+        auth.signOut();
+
+
+// this listener will be called when there is change in firebase user session
+        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LogInActivity.class));
+                    finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+    }
+}
+
+
+       /* // link UI elements to references
+        logInButton = (Button) findViewById(R.id.logInButton);
+        Email = (EditText) findViewById(R.id.emailEditText);
+        Password = (EditText) findViewById(R.id.passwordEditText);
+
+        // Check if user is logged in or not
+
+        //////////////////////////////////////////////////
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openpage2();
+            }
+        });
+    }
+
+    public void openpage2() {
+        Intent intent = new Intent(this, page1.class);
+        startActivity(intent);
+    }
+}
+       // Views
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        // Buttons
+        findViewById(R.id.logInButton).setOnClickListener(this);
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+}
+
+        */
